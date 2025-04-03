16577d9b381ab1040464fd9114f9732b1779c972
Avinash Yadav <avinashyadav06.ay@gmail.com>
2025-04-03 18:57:43 +0530

    Added View Pass(S) feature

--- src/views/pass/Passes.jsx
+++ src/views/pass/Passes.jsx
@@ -17,7 +17,7 @@ const Passes = () => {
     const fetchData = async () => {
         setIsLoading(true);
         try {
-            const response = await fetch(`${url}/passes/visitor-pass-info`, {
+            const response = await fetch(`${url}/passes/pass-info/`, {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
@@ -76,40 +76,34 @@ const Passes = () => {
                     <table className="text-left w-full border-collapse">
                         <thead>
                             <tr>
-                                <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light text-center">Visitor Image</th>
-                                <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Visitor Name</th>
-                                <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Purpose</th>
-                                <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Whom To Visit</th>
-                                <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Visiting Department</th>
-                                <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Created On</th>
-                                <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Valid Until</th>
+                                <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Visitor/Employee Name</th>
+                                <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Pass Type</th>
+                                <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light text-center">Local Pass ID</th>
+                                <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Validity</th>
+                                <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Pass Image</th>
                             </tr>
                         </thead>
                         <tbody>
                             {passesData.map((pass, index) => (
                                 <tr key={index} className="hover:bg-grey-lighter cursor-pointer" onClick={() => handleRowClick(pass)}>
+                                    <td className="py-4 px-6 border-b border-grey-light">{pass.visitor_name || pass.employee_name}</td>
+                                    <td className="py-4 px-6 border-b border-grey-light">{pass.pass_type}</td>
+                                    <td className="py-4 px-6 border-b border-grey-light">{pass.local_pass_id}</td>
+                                    <td className="py-4 px-6 border-b border-grey-light">
+                                        {new Date(pass.validity).toLocaleString('en-IN', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false })}
+                                    </td>
                                     <td className="py-1 px-1 border-b border-grey-light">
                                         <div className="flex justify-center">
                                             <div className="inline-block h-16 w-16 border-2 border-gray-300 rounded-full overflow-hidden bg-customGreen">
-                                                {pass.visitor.image ? (
-                                                    <img src={`data:image/jpeg;base64,${pass.visitor.image}`} alt="User" />
+                                                {pass.pass_image ? (
+                                                    <img src={`data:image/jpeg;base64,${pass.pass_image}`} alt="Pass" />
                                                 ) : (
                                                     <div className="h-full w-full flex items-center justify-center text-white bg-customGreen">
-                                                        {pass.visitor.first_name ? pass.visitor.first_name.charAt(0).toUpperCase() : 'N'}
+                                                        {pass.pass_type ? pass.visitor_name.charAt(0).toUpperCase() : 'N'}
                                                     </div>
                                                 )}
                                             </div>
                                         </div>
-                                    </td>
-                                    <td className="py-4 px-6 border-b border-grey-light">{pass.visitor.first_name} {pass.visitor.last_name}</td>
-                                    <td className="py-4 px-6 border-b border-grey-light">{pass.visiting_purpose}</td>
-                                    <td className="py-4 px-6 border-b border-grey-light">{pass.whom_to_visit}</td>
-                                    <td className="py-4 px-6 border-b border-grey-light">{pass.visiting_department}</td>
-                                    <td className="py-4 px-6 border-b border-grey-light">
-                                        {new Date(pass.created_on).toLocaleString('en-IN', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false })}
-                                    </td>
-                                    <td className="py-4 px-6 border-b border-grey-light">
-                                        {new Date(pass.valid_until).toLocaleString('en-IN', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false })}
                                     </td>
                                 </tr>
                             ))}
--- src/views/pass/ViewPass.jsx
+++ src/views/pass/ViewPass.jsx
@@ -29,11 +29,11 @@ const ViewPass = ({ open, onClose, passData }) => {
                 <div className="col-span-1 p-2 border-r-2 border-gray-300">
                     <div className='flex flex-col items-center space-y-4 pb-2'>
                         <img src={passlogo} alt="Pass Logo" className="h-16 w-32" />
-                        {passData?.visitor.image ? (
-                            <img src={`data:image/jpeg;base64,${passData?.visitor.image}`} alt="User" className="h-32 w-32 border-2 border-gray-300 rounded-xl object-cover" />
+                        {passData?.pass_image ? (
+                            <img src={`data:image/jpeg;base64,${passData?.pass_image}`} alt="Pass" className="h-32 w-32 border-2 border-gray-300 rounded-xl object-cover" />
                         ) : (
                             <div className="h-32 w-32 border-2 border-gray-300 flex items-center justify-center text-white bg-customGreen rounded-xl">
-                                {passData?.visitor.first_name ? passData?.visitor.first_name.charAt(0) : 'N/A'}
+                                {passData.pass_type ? passData.visitor_name.charAt(0).toUpperCase() : 'N/A'}
                             </div>
                         )}
                     </div>
@@ -45,18 +45,13 @@ const ViewPass = ({ open, onClose, passData }) => {
                         </div>
                         <div className="flex">
                             <div className="flex-1 flex flex-col space-y-4 p-2">
-                                <InfoItem label="Name" value={`${passData?.visitor.first_name} ${passData?.visitor.last_name}`} />
-                                <InfoItem label="Type" value={passData?.visitor.visitor_type} />
-                                <InfoItem label="Purpose" value={passData?.visiting_purpose} />
-                            </div>
-                            <div className="flex-1 flex flex-col space-y-4 p-2">
-                                <InfoItem label="To-Visit" value={passData?.whom_to_visit} />
-                                <InfoItem label="Department" value={passData?.visiting_department} />
-                                <InfoItem label="Zones" value={passData?.zone_names.join(", ")} />
+                                <InfoItem label="Name" value={`${passData?.visitor_name || passData.employee_name}`} />
+                                <InfoItem label="Type" value={passData.pass_type} />
+                                <InfoItem label="Pass ID" value={passData?.local_pass_id} />
                             </div>
                         </div>
                         <div className="flex justify-left p-2">
-                            <InfoItem label="Valid-Till" value={new Date(passData?.valid_until).toLocaleString('en-IN', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false })} />
+                            <InfoItem label="Valid-Till" value={new Date(passData?.validity).toLocaleString('en-IN', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false })} />
                         </div>
                     </div>
                 </div>
